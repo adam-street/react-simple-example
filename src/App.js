efe2711fb3d751f1650ccdd7c10ebc1c7ccb0a21
adam-street <adam.street.dev@gmail.com>
2022-06-22 20:05:44 -0500

    adde memos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
+import MemoList from './components/MemoList';
 
 function App() {
-  const nameData = "bob";
+
+  const [input, setInput] = useState("")
+  const [memoList, setMemoList] = useState([])
+
+  function onInputChange(event) {
+    setInput(event.target.value);
+  }
+
+  function onClick() {
+    setMemoList(
+      [...memoList, {date: new Date(), text: input } ]
+    )
+  }
+
   return <div>
-    <Counter name={nameData}></Counter>
+    <Counter name={"bob"} foo="bar"></Counter>
+
+    <input value={input} onChange={onInputChange}/>
+    <button onClick={onClick}>Add</button>
+    <MemoList list={memoList}/>
   </div>
 }
 
